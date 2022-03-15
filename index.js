f00b6d9ac3608dc19cb3471f53aeff11b612378f
mrcraked <89685779+mrcraked@users.noreply.github.com>
2022-03-15 16:36:56 +0900

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const ping = require('minecraft-server-util')
 const {
   Client,
   Intents,
-  RichEmbed
+  Embed
 } = require('discord.js')
 // Create Discord intentions, required in v13
 const intents = new Intents(['GUILDS', 'GUILD_MESSAGES'])
